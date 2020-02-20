739281f103a11ab18d74a12487594039e526b17b
yuri oliveira <oliveirayuri10@hotmail.com>
2020-02-20 04:42:47 -0300

    graficos adicionados no final e time()

--- lzw_codificador_wikiPedia.py
+++ lzw_codificador_wikiPedia.py
@@ -1,26 +1,23 @@
-# -*- coding: utf-8 -*-
+# -*-  coding: iso-8859-1 -*-
+# using Python version 3.7
 """
 created on Mon Dec 23 18:04:19 2019
 @author: Yuri Oliveira
 """
+import time
+import os
+import matplotlib.pyplot as plt
 
 #Verifica se existe o simbolo no dicionario
 #Retorna o seu indice, caso exista.
 def existeNoDic(simbolo):
     if simbolo in dic:
         return dic.index(simbolo)
-    return -1
-
-#Cria o dicionario baseado na mensagem que sera codificada
-#def criaDic(mensagem):
-#    [dic_inicial.append(simbolo) for simbolo in mensagem if simbolo not in dic_inicial]
-    
+    return -1 
 
 #Funcao que completa com zeros um numero binario, baseado em um parametro k (tamanho bin)
 def binario(b, k):
-    binario = bin(b)[2:]
-    while len(binario) < k:
-        binario = '0' + binario
+    binario = bin(b)[2:].zfill(k)
     return binario
 
 #Salva a saida codificada em um arquivo
@@ -38,7 +35,7 @@ def le_saida(k):
     saida_lzw = []
     arq = open("C:/Users/Yuri Oliveira/Desktop/lzw_saida_" + str(k) + ".txt","r+")
     conteudo = arq.read()
-    print(conteudo)
+    #print(conteudo)
     for i in range(int(len(conteudo)/k)):
         saida_lzw.append(int(str(conteudo[i*k:(i+1)*k]), 2))
     arq.close()
@@ -70,7 +67,7 @@ def lzw_decode(leitura):
 
 #Mensagem que será codificada  
 
-mensagem = arq = open("C:/Users/Yuri Oliveira/Desktop/corpus16MB.txt","r+")
+arq = open("C:/Users/Yuri Oliveira/Desktop/corpus16MB.txt","r", encoding = 'iso-8859-1')
 mensagem = arq.read()
 #mensagem = "ABRACADABRA"
 arq.close()
@@ -85,13 +82,16 @@ dic_tamanho = 256
 dicionario = [chr(i) for i in range(dic_tamanho)]
 
 #End of File variable
-EOF = '`'
+EOF = dicionario[3]
 #Junta a mensagem com o EOF
 mensagem = mensagem + EOF
 
+tempos_k = []
 
 #faz para K valendo de 9 a 16
-for k in range(9,10):
+for k in range(12,17):
+    #pega o tempo de execucao
+    tempo_inicial = time.time()
     #Saida Codificada
     saida = []
     #variaveis auxiliar
@@ -118,18 +118,45 @@ for k in range(9,10):
         else:
             saida.append(existeNoDic(I))
         
-    print("saidas utilizando k = {}".format(k))
+    #print("saidas utilizando k = {}".format(k))
     #print("Dicionario Final:")
     #print(dic)
-    print("Saida Codificada:")
-    print(saida)
+    #print("Saida Codificada:")
+    #print(saida)
     
-    
+    tempos_k.append(time.time() - tempo_inicial)
     salva_saida(saida, k)
-    saida_lida = le_saida(k)
-    print(saida_lida)
+    #saida_lida = le_saida(k)
+    #print(saida_lida)
 
 
+#graficos
+'''    
+statinfo = os.stat("C:/Users/Yuri Oliveira/Desktop/corpus16MB.txt")
+tamanho_corpus = statinfo.st_size
+
+tamanho_saidas = []
+for i in range(9,14):
+    statinfo = os.stat("C:/Users/Yuri Oliveira/Desktop/lzw_saida_"+ str(i) + ".txt")
+    tamanho_saidas.append(int(statinfo.st_size/8))
+
+#RC - Razão de Compressão (tamanho original / tamanho comprimido)
+RC = [tamanho_corpus/ts for ts in tamanho_saidas]
+
+#plt.bar(list(range(9,14)), RC, color = 'blue')
+plt.title('RC x K')
+plt.ylabel('Razão de Compressão')
+plt.xlabel('K')
+plt.plot(list(range(9,14)), RC)
+plt.grid(True)
 
 
+#Grafico tempo x k
+plt.title('tempo x K')
+plt.ylabel('tempo')
+plt.xlabel('K')
+plt.plot(list(range(9,14)), tempos_k)
+plt.grid(True)
+
+'''
 
